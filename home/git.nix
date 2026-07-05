1858df60a5bd8f543f779824dddfde87df777499
Jack Rhoa <dev@jackrhoa.com>
2026-07-05 10:20:54 -0400

    nix git

--- home/git.nix
+++ home/git.nix
@@ -1,5 +1,30 @@
-{
+{ config, ... }: {
   programs.git = {
     enable = true;
+    lfs.enable = true;
+    settings = {
+      user = {
+        name = "Jack Rhoa";
+        email = "dev@jackrhoa.com";
+      };
+      core = {
+        editor = "nvim";
+        autocrlf = "input";
+        pager = "less";
+      };
+      init.defaultBranch = "main";
+      alias = {
+        st = "status";
+        co = "checkout";
+        ci = "commit";
+      };
+      gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
+      push.autosetupremote = true;
+    };
+      signing = {
+        key = "${config.home.homeDirectory}/.ssh/git_key";
+        format = "ssh";
+        signByDefault = true;
+      };
   };
 }
