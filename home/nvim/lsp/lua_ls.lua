58522b624d89a59b03c2bb2c08331fae7162b8ed
Jack Rhoa <dev@jackrhoa.com>
2026-07-15 00:58:48 -0400

    feat(nvim): add folder-scoped workspace library for lsps
    This replaces global keyword diagnostics replacement of vim with its workspace.library version only in \nhome/nvim/ and adds same functionality for hl (hyprland) in home/hypr/

--- home/nvim/lsp/lua_ls.lua
+++ home/nvim/lsp/lua_ls.lua
@@ -1,12 +1,5 @@
 return {
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
-  root_markers = { '.luarc.json', '.luarc.jsonc', '.git' },
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { 'vim' },
-      },
-    },
-  },
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.git' }
 }
