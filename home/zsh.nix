eb609fff63690ee0e64530c4050f921afa181691
Jack Rhoa <dev@jackrhoa.com>
2026-08-13 18:08:42 -0400

    fix(zsh): stop sharing history between panes

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,15 +1,30 @@
 { pkgs, ... }: {
   programs.zsh = {
       enable = true;
+      history.share = false;
   };
 
   programs.zsh.shellAliases = {
     whatismyip = "curl ipinfo.io";
+<<<<<<< Updated upstream
      
     ls = "eza --icons=auto";
     ll = "eza -l --icons=auto --git --header";
     la = "eza -la --icons=auto --git";
     lt = "eza --tree --level=2 --icons=auto --ignore-glob 'node_modules'";
+||||||| Stash base
+     
+    ls = "eza --icons";
+    ll = "eza -l --icons --git --header";
+    la = "eza -la --icons --git";
+    lt = "eza --tree --level=2 --icons";
+=======
+         
+    ls = "eza --icons";
+    ll = "eza -l --icons --git --header";
+    la = "eza -la --icons --git";
+    lt = "eza --tree --level=2 --icons";
+>>>>>>> Stashed changes
     
     g = "git";
   };
