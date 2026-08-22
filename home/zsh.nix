e6cb069f756f3115a22b93123757dfc6a7d9ef2f
Jack Rhoa <dev@jackrhoa.com>
2026-08-22 12:37:50 -0400

    feat(eza): use --collapse in lt alias

--- home/zsh.nix
+++ home/zsh.nix
@@ -10,7 +10,7 @@
     ls = "eza --icons=auto";
     ll = "eza -l --icons=auto --git --header";
     la = "eza -la --icons=auto --git";
-    lt = "eza --tree --level=3 --icons=auto --ignore-glob 'node_modules'";
+    lt = "eza --tree --level=3 --icons=auto --collapse 'node_modules'";
     
     g = "git";
   };
