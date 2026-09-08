6b16f90709eea5521b2e4c5569b2d96ff6d5978e
Jack Rhoa <dev@jackrhoa.com>
2026-09-08 13:43:02 -0400

    fix(starship): remove extra space

--- home/starship.nix
+++ home/starship.nix
@@ -25,19 +25,18 @@
             )
           '
         '';
-        symbol = " ";
         style = "bold purple";
-        format = "on [$symbol$output]($style) ";
+        format = "on [$output]($style) ";
       };
 
+      # Show git iff jj doesn't exist
       custom.git = {
         when = "! jj root --ignore-working-copy && git rev-parse --is-inside-work-tree";
         command = ''
           echo "$(git branch --show-current | grep . || git rev-parse --short HEAD)$(git status --porcelain | grep -q . && echo ' *')"
         '';
-        symbol = " ";
         style = "bold purple";
-        format = "on [$symbol$output]($style) ";
+        format = "on [$output]($style) ";
       };
     };
   };
