919538dea5425330b6459361699b4dceb10d62ad
Jack Rhoa <dev@jackrhoa.com>
2026-09-08 11:08:03 -0400

    feat(starship): integrate jj

--- home/starship.nix
+++ home/starship.nix
@@ -2,11 +2,43 @@
   programs.starship = {
     enable = true;
     settings = {
-      # time.disabled = false;
+      command_timeout = 1000;
+
       gcloud.disabled = true;
       python.disabled = true;
       nodejs.disabled = true;
       package.disabled = true;
+
+      git_branch.disabled = true;
+      git_commit.disabled = true;
+      git_state.disabled = true;
+      git_status.disabled = true;
+
+      custom.jj = {
+        when = "jj root --ignore-working-copy";
+        command = ''
+          jj log --revisions @ --no-graph --color never --template '
+            separate(" ",
+              coalesce(bookmarks.join(" "), change_id.shortest()),
+              if(empty, "(empty)"),
+              coalesce(description.first_line(), "(no description set)"),
+            )
+          '
+        '';
+        symbol = " ";
+        style = "bold purple";
+        format = "on [$symbol$output]($style) ";
+      };
+
+      custom.git = {
+        when = "! jj root --ignore-working-copy && git rev-parse --is-inside-work-tree";
+        command = ''
+          echo "$(git branch --show-current | grep . || git rev-parse --short HEAD)$(git status --porcelain | grep -q . && echo ' *')"
+        '';
+        symbol = " ";
+        style = "bold purple";
+        format = "on [$symbol$output]($style) ";
+      };
     };
   };
 }
